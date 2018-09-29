2159226c909cd6331ee8944ef2045b21c15dd762
huzijian <huzijian@meituan.com>
2018-09-29 19:00:41 +0800

    提交一版

---
+++ netty-demo/src/main/java/nio/NettyOio.java
@@ -0,0 +1,16 @@
+package nio;
+
+import io.netty.bootstrap.Bootstrap;
+
+/**
+ * @Author: ZiJian Hu
+ * @Description:
+ * @Date: Created in 2:17 PM 2018/9/18
+ * @Modified By:
+ */
+public class NettyOio {
+
+    public static void main(String[] args) {
+        Bootstrap bootstrap = new Bootstrap();
+    }
+}
